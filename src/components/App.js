7cea268f8773ef39380f5e6ccf60f0a84aae65c8
LastAngelic <angelpenagonzalez@hotmail.com>
2022-05-06 13:26:41 -0500

    Charts

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,16 @@ import Header from '../shared/layout/Header';
 import Footer from '../shared/layout/Footer';
 import Content from '../shared/layout/Content';
 // import Todo from './Todo/Todo';
-import Timer from './Pomodoro/Timer';
-import Crypto from './Crypto';
+// import Timer from './Pomodoro/Timer';
+// import Crypto from './Crypto';
+import ChartHome from './Chart/ChartHome';
 
 function App() {
   return (
     <div className="App">
       <Header title="Header" url="https://www.youtube.com/watch?v=dQw4w9WgXcQ" />
       <Content>
-        <Crypto />
+        <ChartHome />
       </Content>
       <Footer />
     </div>
