8d07d20191dd7d24f697aac78d910b0aab77f23e
LastAngelic <angelpenagonzalez@hotmail.com>
2022-05-09 08:59:24 -0500

    Cryto fix

--- src/components/App.js
+++ src/components/App.js
@@ -5,15 +5,15 @@ import Footer from '../shared/layout/Footer';
 import Content from '../shared/layout/Content';
 // import Todo from './Todo/Todo';
 // import Timer from './Pomodoro/Timer';
-// import Crypto from './Crypto';
-import ChartHome from './Chart/ChartHome';
+import Crypto from './Crypto';
+// import ChartHome from './Chart/ChartHome';
 
 function App() {
   return (
     <div className="App">
       <Header title="Header" url="https://www.youtube.com/watch?v=dQw4w9WgXcQ" />
       <Content>
-        <ChartHome />
+        <Crypto />
       </Content>
       <Footer />
     </div>
