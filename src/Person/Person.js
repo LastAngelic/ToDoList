4fec02a34b7309bdf6d1635d595703742a35fd6f
LastAngelic <angelpenagonzalez@hotmail.com>
2022-05-27 20:21:01 -0500

    Formulario Person

---
+++ src/Person/Person.js
@@ -0,0 +1,134 @@
+import React, { Component } from 'react';
+import './Person.css';
+
+class Person extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            Name: '',
+            LastName: '',
+            Email: '',
+            Phone: '',
+
+            errors: {
+                Name: false,
+                LastName: false
+            }
+        }
+    }
+    
+    handleOnChange = e => {
+        const {target: {value, name}} = e;
+        
+        this.setState({
+            [name]: value
+        })
+
+        console.log(value)
+    }
+    
+    handleOnSubmit = e => {
+        e.preventDefault();
+        const {Name, LastName, Email, Phone} = this.state;
+        
+        this.setState({
+            errors: {
+                Name: Name.trim() === '',
+                LastName: LastName.trim() === ''
+            }
+        })
+
+        if (Name && LastName) {
+            const data = {
+                Name, 
+                LastName, 
+                Email, 
+                Phone
+            }
+    
+            console.log('Data: ', data);
+        }
+    }
+
+    render() {
+        return (
+            <div className='Person'>
+                <form onSubmit={this.handleOnSubmit}>
+                    <div>
+                        <label>
+                            <p><strong>Name: </strong></p>
+                            <input 
+                                name="Name"
+                                type="text" 
+                                value={this.state.Name}
+                                onChange = {this.handleOnChange}
+                                className={
+                                    this.state.errors.Name ? 'error': ''
+                                }
+                            />
+                        </label>
+                        {
+                            this.state.errors.Name
+                            &&
+                            <div className='errorMessage'>Required</div>
+                        }
+                    </div>
+                    <div>
+                        <label>
+                            <p><strong>Last Name: </strong></p>
+                            <input 
+                                name="LastName"
+                                type="text" 
+                                value={this.state.LastName}
+                                onChange = {this.handleOnChange}
+                                className={
+                                    this.state.errors.LastName ? 'error': ''
+                                }
+                            />
+                        </label>
+                        {
+                            this.state.errors.LastName
+                            &&
+                            <div className='errorMessage'>Required</div>
+                        }
+                    </div>
+                    <div>
+                        <label>
+                            <p><strong>Email: </strong></p> 
+                            <input 
+                                name="Email"
+                                type="email" 
+                                value={this.state.Email}
+                                onChange = {this.handleOnChange}
+                                className={
+                                    this.state.errors.LastName ? 'error': ''
+                                }
+                            />
+                        </label>
+                        {
+                            this.state.errors.LastName
+                            &&
+                            <div className='errorMessage'>Required</div>
+                        }
+                    </div>
+                    <div>
+                        <label>
+                            <p><strong>Phone: </strong></p> 
+                            <input 
+                                name="Phone"
+                                type="tel" 
+                                value={this.state.Phone}
+                                onChange = {this.handleOnChange}
+                            />
+                        </label>
+                    </div>
+                    <div>
+                        <button>Submit</button>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Person;
--- src/components/App.js
+++ src/components/App.js
@@ -9,14 +9,15 @@ import Content from '../shared/layout/Content';
 // import ChartHome from './Chart/ChartHome';
 //import Animation from './Animating/Animation';
 //import Numbers from './PureComponent/Number';
-import Calculator from './Calculator/Calculator';
+//import Calculator from './Calculator/Calculator';
+import Person from '../Person/Person';
 
 function App() {
   return (
     <div className="App">
       <Header title="Number" url="https://www.youtube.com/watch?v=dQw4w9WgXcQ" />
       <Content>
-        <Calculator />
+        <Person />
       </Content>
       <Footer />
     </div>
